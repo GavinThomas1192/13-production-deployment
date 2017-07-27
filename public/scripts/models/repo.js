cb6111d9b77d405899a55ad5b6eb9dc7443251ff
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-27 09:00:14 -0700

    fixed authentication

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -12,7 +12,7 @@ var app = app || {};
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
 
-    $.get('/github/user/repos?type=owner')
+    $.get('/github/user/repos')
       .then(
         data => {
           repos.all = data, err => console.error(err)
